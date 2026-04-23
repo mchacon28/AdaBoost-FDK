58eefe57440d155430547507d0a274b331308d8f
Jose M. Moyano <jmoyano1@us.es>
2026-04-23 13:08:09 +0200

    Adding overall_AUC_score() method to AdaBoostFKD, so the AUC can be also obtained for Local AdaBoost base version.

--- models/AdaBoostFKD.py
+++ models/AdaBoostFKD.py
@@ -924,6 +924,140 @@ class AdaBoostFKD:
                  'local_difference_w': local_difference_w, 'global_difference_w': global_difference_w}
             )
 
+    def overall_AUC_score(self, X_global, y_global, macro_AUC=False):
+        """
+        Obtain the overall AUC of the federated model and the only local ones, using either the client's
+            local data and the global data received as parameter.
+
+        Args:
+            X_global (np.array): 2-d array to be predicted.
+            y_global (np.array): 1-d array representing X_global true labels.
+            macro_AUC (bool): Set it to True if AUC macro average wants to be calculated in addition to weighted
+            average. Set to False by default.  
+
+        Returns: Dataframe with a row for each client and columns:
+            - data_distrib: Data of each client
+            - FL_AUC_own_data: AUC of the federated model with each client's data
+            - FL_AUC_global_data: AUC of the federated model with the data given as input
+            - local_wAUC_own_data: AUC of the local Adaboost model with each client's data
+            - local_wAUC_global_data: AUC of the local Adaboost model with data given as input
+            - local_difference_w: Difference between the federated and local models on local data
+            - global_difference_w: Difference between the federated and global models on global data
+        """
+        FL_wAUC_own_data = np.zeros(self.n_clients)
+        FL_wAUC_global_data = np.zeros(self.n_clients)
+        local_wAUC_own_data = np.zeros(self.n_clients)
+        local_wAUC_global_data = np.zeros(self.n_clients)
+        global_difference_w = np.zeros(self.n_clients)
+        local_difference_w = np.zeros(self.n_clients)
+
+        if macro_AUC:
+            FL_maAUC_own_data = np.zeros(self.n_clients)
+            FL_maAUC_global_data = np.zeros(self.n_clients)
+            local_maAUC_own_data = np.zeros(self.n_clients)
+            local_maAUC_global_data = np.zeros(self.n_clients)
+            global_difference_ma = np.zeros(self.n_clients)
+            local_difference_ma = np.zeros(self.n_clients)
+
+        for i in range(self.n_clients):
+            if i not in self.attackers:
+                own_data_Xtest, own_data_ytest = self.test_clients_data[i]
+                local_model = self.local_clients_models_dict[i]
+
+                fl_prob_own = self.client_predict_data(own_data_Xtest[:, :-1], i, soft_predictions=True)
+                fl_prob_global = self.client_predict_data(X_global, i, soft_predictions=True)
+                local_prob_own = local_model.predict(own_data_Xtest[:, :-1], soft_predictions=True)
+                local_prob_global = local_model.predict(X_global, soft_predictions=True)
+                
+                if fl_prob_own.shape[1] == 2:
+                    fl_prob_own = fl_prob_own[:, 1]
+                    fl_prob_global = fl_prob_global[:, 1]
+                if local_prob_own.shape[1] == 2:
+                    local_prob_own = local_prob_own[:, 1]
+                    local_prob_global = local_prob_global[:, 1]
+
+                FL_wAUC_own_data[i] = roc_auc_score(own_data_ytest, fl_prob_own, 
+                                        multi_class='ovr', average='weighted', labels=np.unique(own_data_ytest))
+                                    # f1_score(self.client_predict_data(own_data_Xtest[:, :-1], i), own_data_ytest,
+                                    #         labels=np.unique(own_data_ytest), average='weighted', zero_division=0.0)
+                FL_wAUC_global_data[i] = roc_auc_score(y_global, fl_prob_global, 
+                                            multi_class='ovr', average='weighted', labels=np.unique(y_global))
+
+                if np.isnan(local_prob_own).any() or np.isnan(local_prob_global).any():
+                    local_wAUC_own_data[i] = np.nan
+
+                    local_wAUC_global_data[i] = np.nan
+
+                    global_difference_w[i] = np.nan
+                    local_difference_w[i] = np.nan
+
+                else:
+                    local_wAUC_own_data[i] = roc_auc_score(own_data_ytest, local_prob_own, 
+                                                multi_class='ovr', average='weighted', labels=np.unique(own_data_ytest))
+
+                    local_wAUC_global_data[i] = roc_auc_score(y_global, local_prob_global, 
+                                                    multi_class='ovr', average='weighted', labels=np.unique(y_global))
+
+                    global_difference_w[i] = FL_wAUC_global_data[i] - local_wAUC_global_data[i]
+                    local_difference_w[i] = FL_wAUC_own_data[i] - local_wAUC_own_data[i]
+
+                if macro_AUC:
+                    FL_maAUC_own_data[i] = roc_auc_score(own_data_ytest, fl_prob_own, 
+                                        multi_class='ovr', average='macro', labels=np.unique(own_data_ytest))
+                    FL_maAUC_global_data[i] = roc_auc_score(y_global, fl_prob_global, 
+                                                multi_class='ovr', average='macro', labels=np.unique(y_global))
+
+                    if np.isnan(local_prob_own).any() or np.isnan(local_prob_global).any():
+                        local_wAUC_own_data[i] = np.nan
+
+                        local_wAUC_global_data[i] = np.nan
+
+                        global_difference_w[i] = np.nan
+                        local_difference_w[i] = np.nan
+                    else:
+                        local_maAUC_own_data[i] = roc_auc_score(own_data_ytest, local_prob_own, 
+                                                    multi_class='ovr', average='macro', labels=np.unique(own_data_ytest))
+
+                        local_maAUC_global_data[i] = roc_auc_score(y_global, local_prob_global, 
+                                                    multi_class='ovr', average='macro', labels=np.unique(y_global))
+                        global_difference_ma[i] = FL_maAUC_global_data[i] - local_maAUC_global_data[i]
+                        local_difference_ma[i] = FL_maAUC_own_data[i] - local_maAUC_own_data[i]
+            else:
+                # The score of the attacker clients is not considered to assess the performance of the clients' models
+                FL_wAUC_own_data[i] = np.nan
+                FL_wAUC_global_data[i] = np.nan
+                local_wAUC_own_data[i] = np.nan
+                local_wAUC_global_data[i] = np.nan
+                global_difference_w[i] = np.nan
+                local_difference_w[i] = np.nan
+
+                if macro_AUC:
+                    FL_maAUC_own_data[i] = np.nan
+                    FL_maAUC_global_data[i] = np.nan
+                    local_maAUC_own_data[i] = np.nan
+                    local_maAUC_global_data[i] = np.nan
+                    global_difference_ma[i] = np.nan
+                    local_difference_ma[i] = np.nan
+
+        if macro_AUC:
+            return pd.DataFrame(
+                {'data_distrib': self.number_data_clients,
+                 'FL_wAUC_own_data': FL_wAUC_own_data, 'FL_wAUC_global_data': FL_wAUC_global_data,
+                 'local_wAUC_own_data': local_wAUC_own_data, 'local_wAUC_global_data': local_wAUC_global_data,
+                 'local_difference_w': local_difference_w, 'global_difference_w': global_difference_w,
+                 'FL_maAUC_own_data': FL_maAUC_own_data, 'FL_maAUC_global_data': FL_maAUC_global_data,
+                 'local_maAUC_own_data': local_maAUC_own_data, 'local_maAUC_global_data': local_maAUC_global_data,
+                 'local_difference_ma': local_difference_ma, 'global_difference_ma': global_difference_ma,
+                 }
+            )
+        else:
+            return pd.DataFrame(
+                {'data_distrib': self.number_data_clients,
+                 'FL_wAUC_own_data': FL_wAUC_own_data, 'FL_wAUC_global_data': FL_wAUC_global_data,
+                 'local_wAUC_own_data': local_wAUC_own_data, 'local_wAUC_global_data': local_wAUC_global_data,
+                 'local_difference_w': local_difference_w, 'global_difference_w': global_difference_w}
+            )
+
     def clients_score_dataframes(self, scheme, global_Xtest, targets):
         """
         Calculates the scores of each client, depending on the scheme. For each client
